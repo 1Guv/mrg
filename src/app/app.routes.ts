68e67223b3f4f07f179af44bb0bd66b396358434
Gurvinder Singh Sandhu <Gurvinder.Singh.Sandhu@gmail.com>
2026-04-05 16:42:40 +0100

    feat: add list-plate and list-plate/success routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { RegisterComponent } from './core/register/register.component';
 import { authGuard } from './guards/auth.guard';
 import { AccountDashboardComponent } from './core/account-dashboard/account-dashboard.component';
 import { ForgotPasswordComponent } from './core/forgot-password/forgot-password.component';
+import { PlatesForSaleComponent } from './core/plates-for-sale/plates-for-sale.component';
+import { ListPlateComponent } from './core/list-plate/list-plate.component';
+import { ListPlateSuccessComponent } from './core/list-plate-success/list-plate-success.component';
 
 export const routes: Routes = [
     { path: '', component: MainSectionComponent },
@@ -16,4 +19,7 @@ export const routes: Routes = [
         canActivate: [authGuard]
     },
     { path: 'forgot-password', component: ForgotPasswordComponent },
+    { path: 'plates-for-sale', component: PlatesForSaleComponent },
+    { path: 'list-plate', component: ListPlateComponent },
+    { path: 'list-plate/success', component: ListPlateSuccessComponent },
 ];
